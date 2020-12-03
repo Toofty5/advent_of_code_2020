c84c71b4acedad12420e112f6f5cc1872ff6a850
Jay <jay.cheung@gmail.com>
2020-12-03 10:42:14 -0500

    Need to get used to commenting

--- 1_expense_report.py
+++ 1_expense_report.py
@@ -1,5 +1,8 @@
-input_file = open("input.txt", 'r')
 
+# removed .txt extension from windows
+input_file = open("input", 'r')
+
+"""
 entries = []
 
 this_line = input_file.readline().rstrip()
@@ -7,7 +10,12 @@ this_line = input_file.readline().rstrip()
 while this_line != '':
     entries.append(int(this_line));
     this_line = input_file.readline().rstrip()
+"""
 
+# changed up the file input for some list comprehension fun
+entries = [int(line) for line in input_file.readlines()]
+
+# Since we only traverse once, using pop instead of keeping a pointer
 while entries != []:
     this_number = entries.pop(0);
     for other_number in entries:
