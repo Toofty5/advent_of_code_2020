ca869e6b4004752dd4d5ec7b560eba06574fab5e
Jay <jay.cheung@gmail.com>
2020-12-08 09:24:47 -0500

    Redoing day 8 from branch

--- 4_passport.py
+++ 4_passport.py
@@ -3,21 +3,26 @@ lines = open("input", 'r').readlines()
 
 entries = [] 
 
+# Needs cleaning up: 
+
 this_line = (lines.pop()).rstrip()
 while len(lines) > 0:
 
     this_entry = {}
     while this_line != '':
         line_fields = this_line.rsplit()
-
+        
         for field in line_fields:
             key, value = field.rsplit(':')
-            this_entry[key] = value
-        # print(this_entry)
+            this_entry[key] = value  # there's got to be a better way
+
         if(len(lines) > 0):
             this_line =(lines.pop()).rstrip()
         else:
             this_line = ''
+
+        this_entry = {key:val for (key, val) for field.split(':') in this_line.split()}
+
     print(this_entry)
     entries.append(this_entry)
 
