103dde0fcaa450ba74625ff2d5b1bbbe31d0ced5
Jay <jay.cheung@gmail.com>
2020-12-09 11:04:20 -0500

    fixed handheld

--- 8a_handheld.py
+++ 8a_handheld.py
@@ -1,28 +1,47 @@
 lines = [line.strip() for line in open("input", "r").readlines()]
 
-visited = []
-accumulator = 0
+for line_num, line in enumerate(lines):
+
+    swap_command, swap_val_str = line.split()
+
+    if swap_command == "nop":
+        lines[line_num] = "jmp " + swap_val_str
+    if swap_command == "jmp":
+        lines[line_num] = "nop " + swap_val_str
+
+    visited = []
+    accumulator = 0
 
 
-pointer = 0;
+    pointer = 0;
 
-while pointer not in visited:
-    visited.append(pointer)
-    
-    command, val_str = lines[pointer].split()
+    while pointer not in visited:
+        if pointer >= len(lines):
+            print(f"End of commands.  Accumulator: {accumulator}")
+            break
 
-    val = int(val_str)
+        visited.append(pointer)
 
-    if command == "nop":
-        pointer += 1
-    elif command == "acc":
-        accumulator += val
-        pointer += 1
-    elif command == 'jmp':
-        pointer += val
-    else:
-        print("what")
-print(accumulator)
+        command, val_str = lines[pointer].split()
+
+        val = int(val_str)
+
+        if command == "nop":
+            pointer += 1
+        elif command == "acc":
+            accumulator += val
+            pointer += 1
+        elif command == 'jmp':
+            pointer += val
+        else:
+            print("what")
+    print(accumulator)
+
+    # put the command back
+    if swap_command == "nop":
+        lines[line_num] = "nop " + swap_val_str
+    if swap_command == "jmp":
+        lines[line_num] = "jmp " + swap_val_str
 
 
 
