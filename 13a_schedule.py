5cd825ee1718a54d0795d8a8b6dc13cc37fc87d2
Jay <jay.cheung@gmail.com>
2021-01-06 21:20:41 -0500

    FINALLY finished day 13!

--- 13a_schedule.py
+++ 13a_schedule.py
@@ -5,10 +5,22 @@ timestamp = int(timestamp)
 
 bus_lines = second_line.split(',')
 
-open_lines = [int(thing) for thing in bus_lines if thing != 'x']
-print(open_lines)
+open_lines = [thing if "x" else int(thing) for thing in bus_lines]
 
-product = 1
-for thing in open_lines:
-    product = product * thing
-print(product)
+offset_lines = [(x, int(y)) for (x,y) in enumerate(open_lines) if y != "x"]
+
+zero_offset, this_bus = offset_lines[0]
+this_time = this_bus
+this_jump = this_bus
+
+print(offset_lines)
+
+for next_offset, next_bus in offset_lines[1:]:
+    while True:
+        if (this_time + next_offset) % next_bus == 0:
+            print(this_time)
+            this_jump = this_jump * next_bus
+            break
+        this_time += this_jump
+
+            
